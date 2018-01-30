19eb33498b532fa4927b275680c57fc58231d9a0
malcolmessaid <malcolmessaid1@gmail.com>
2018-01-29 22:41:47 -0500

    Daily Commit 1/29                   Did more research on Chrome extensions and selection objects. Began writing code in my content.js file. Working on figuring out exactly what I will do.

--- content.js
+++ content.js
@@ -1,6 +1,19 @@
-
-
+// Import JQuery
 var script = document.createElement('script');
 script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
+
+
+var getSelectionData = function (){
+  var sel = window.getSelection();
+  var temp = selection.anchorNode;
+  console.log(sel.toString())
+}
+
+$(document).ready(function(){
+  $(document).dblclick( function(){
+    console.log('fs')
+    getSelectionData()
+  });
+});
